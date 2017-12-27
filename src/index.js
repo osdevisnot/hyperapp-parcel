884cf6f71e40266935a655b2e3a05adae7153193
osdevisnot <osdevisnot@gmail.com>
2017-12-27 11:59:04 +0530

    use hyperapp with parcel

---
+++ src/index.js
@@ -0,0 +1,26 @@
+import { h, app } from 'hyperapp';
+
+const state = {
+  count: 0
+};
+
+const actions = {
+  down: () => state => ({ count: state.count - 1 }),
+  up: () => state => ({ count: state.count + 1 })
+};
+
+const view = (state, actions) => (
+  <main class="wrapper">
+    <h1 class="title">{state.count}</h1>
+    <div class="actions">
+      <button onclick={actions.down} class="minus">
+        -
+      </button>
+      <button onclick={actions.up} class="plus">
+        +
+      </button>
+    </div>
+  </main>
+);
+
+export const main = app(state, actions, view, document.body);
